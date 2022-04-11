b74bc4a1738d126488855a53e538c6ccbd68d361
JeongHyeon Kim <jeonghyeon.kim@creatrip.com>
2022-04-11 18:05:48 +0900

    add: Comment:

--- src/index.ts
+++ src/index.ts
@@ -8,34 +8,35 @@ type GetChildClassProperties<T1 extends T2, T2> = NonFunctionProperties<
   Omit<T1, keyof T2>
 >;
 
-abstract class BaseEntity<T extends BaseEntity<T> = any> {
+abstract class BaseEmbeddedEntity<T extends BaseEmbeddedEntity<T> = any> {
   id: number = 1;
 
   create() {}
 
-  constructor(properties: GetChildClassProperties<T, BaseEntity<T>>) {
+  constructor(properties: GetChildClassProperties<T, BaseEmbeddedEntity<T>>) {
     Object.assign(this, properties);
   }
 }
 
-class User extends BaseEntity<User> {
+class User extends BaseEmbeddedEntity<User> {
   name: string;
 }
 
 console.log(
   new User({
     name: "rhea-so",
+    age: 22, // <- User Property가 아니어서 에러남. 제거시 빌드 성공.
   })
 );
 
-// BaseEntity의 제네릭에 아무것도 안넣으면 기본 값 any
-class Any extends BaseEntity {
+// BaseEmbeddedEntity의 제네릭에 아무것도 안넣으면 기본 값 any
+class Any extends BaseEmbeddedEntity {
   name: string;
 }
 
 console.log(
   new Any({
     name: "rhea-so",
-    hi: "",
+    age: 22,
   })
 );
